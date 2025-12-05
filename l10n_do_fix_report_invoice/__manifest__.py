a4d5b89ae989290bffd105ecd69357487b14d0f1
garibaldy <davidcv9@gmail.com>
2025-12-05 22:01:34 +0000

    fix in footer qr information for ECF

--- l10n_do_fix_report_invoice/__manifest__.py
+++ l10n_do_fix_report_invoice/__manifest__.py
@@ -1,23 +1,9 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "fix invoice report for Indexa localization Electronic Invoices",
-
-    'summary': """
-        fix electronic invoices format
-    """,
-
-    'description': """
-        Dominican Republic Fiscal electronic invoice format fix
-        =====================================================
-
-    """,
-
-    'author': "David Contreras (Garibaldy)",
-    'website': "https://www.intrepidux.com",
-    'category': 'Accounting/Localizations/Account Charts',
-    'version': '17.0.0.0.3',
-    'depends': [ 'l10n_do_webpos_fe_base'],
+    'name': 'L10n Do Fix Report Invoice',
+    'version': '1.1',
+    'depends': ['l10n_do_accounting'],
     'data': [
+
         'views/report_invoice_fix.xml',
     ],
 }

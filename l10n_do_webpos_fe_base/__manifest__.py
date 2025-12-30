a0bc576a277defe4381cea294c8e6c06a8857979
garibaldy <davidcv9@gmail.com>
2025-12-30 16:12:37 +0000

    some updates

--- l10n_do_webpos_fe_base/__manifest__.py
+++ l10n_do_webpos_fe_base/__manifest__.py
@@ -11,7 +11,7 @@
     'version': "17.0.1.0.24",
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','account_debit_note','l10n_do_accounting'],
+    'depends': ['base','account','l10n_do_accounting'],
 
     # always loaded
    'data': [

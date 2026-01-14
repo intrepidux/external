e8f1422ac1e36317467d1569d41e3374318c55e6
garibaldy <davidcv9@gmail.com>
2026-01-14 19:00:13 +0000

    updating sequence manageing for AN localization

--- l10n_do_fix_report_invoice_an/__manifest__.py
+++ l10n_do_fix_report_invoice_an/__manifest__.py
@@ -1,9 +1,8 @@
 {
-    'name': 'L10n Do Fix Report Invoice, indexa',
+    'name': 'L10n Do Fix Report Invoice, AN',
     'version': '1.1',
-    'depends': ['l10n_do_accounting'],
+    'depends': ['l10n_do_webpos_fe_base'],
     'data': [
-
         'views/report_invoice_fix.xml',
     ],
 }

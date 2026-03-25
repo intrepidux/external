d0bcb58a88c180bf2ae0d1d097e17796ecb9c594
Garibaldy <davidcv9@gmail.com>
2026-03-25 21:09:50 +0000

    using webpos fields for qr presentation

--- l10n_do_fix_report_invoice/__manifest__.py
+++ l10n_do_fix_report_invoice/__manifest__.py
@@ -5,8 +5,8 @@
     "license": "OPL-1",
     "support": "soporte@intrepidux.com",
     'category': 'Localization',
-    'version': "18.0.1.0.2",
-    'depends': ['l10n_do_accounting'],
+    'version': "18.0.1.0.3",
+    'depends': ['l10n_do_webpos_fe_base', ],
     'data': [
 
         'views/report_invoice_fix.xml',

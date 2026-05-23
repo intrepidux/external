b75b1f2b6226a1bf51a11d72cc1284d9a151b7ef
Garibaldy <davidcv9@gmail.com>
2026-05-23 12:21:53 +0000

    revert l10n_do_fix_report_invoice to 18.0.1.0.3 (pre models/wizard)

--- l10n_do_fix_report_invoice/__manifest__.py
+++ l10n_do_fix_report_invoice/__manifest__.py
@@ -1,16 +1,14 @@
 {
-    'name': 'L10n Do Fix Report Invoice, yasmany',
+    'name': 'L10n Do Fix Report Invoice, indexa',
     'author': "Intrepidux SRL, David Contreras",
     'website': "http://www.intrepidux.com",
     "license": "OPL-1",
     "support": "soporte@intrepidux.com",
     'category': 'Localization',
-    'version': "18.0.1.0.4",
+    'version': "18.0.1.0.3",
     'depends': ['l10n_do_webpos_fe_base', ],
     'data': [
-        # 'security/ir.model.access.csv', 
+
         'views/report_invoice_fix.xml',
-        'wizard/account_move_reversal_inherit_views.xml',
-        'views/account_journal_views.xml',
     ],
 }

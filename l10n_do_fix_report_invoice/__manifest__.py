e93dc67def83e42c949feecea1f5d50a181d6b63
Garibaldy <davidcv9@gmail.com>
2026-09-23 13:43:07 +0000

    minimize headers font sizes to webpos sample

--- l10n_do_fix_report_invoice/__manifest__.py
+++ l10n_do_fix_report_invoice/__manifest__.py
@@ -5,7 +5,7 @@
     "license": "OPL-1",
     "support": "soporte@intrepidux.com",
     'category': 'Localization',
-    'version': "18.0.1.0.27",
+    'version': "18.0.1.0.29",
     'depends': [
         'web',
         'account',
